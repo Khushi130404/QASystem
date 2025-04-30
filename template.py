c4292df5195f61e03895f5fe4305ace85de46853
Khushi 💜 <khushipatel130404@gmail.com>
2025-04-30 03:34:19 +0000

    Template

---
+++ template.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    "QAWithPDF/__init__.py",
+    "QAWithPDF/data_ingestion.py",
+    "QAWithPDF/embedding.py",
+    "QAWithPDF/model_api.py",
+    "Experiments/experiment.ipynb",
+    "StreamlitApp.py",
+    "logger.py",
+    "exception.py",
+    "setup.py"]
+
+for filepath in list_of_files:
+    filepath = Path(file_path)
+    filedir, filename = os.path.split(filepath)
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Directory {filedir} created for the file {filename}")
+    
+    if (not os.path.exists(filepath) or os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Empty File {filename} created in {filedir}")
+            
+    else:
+        logging.info(f"File {filename} already exists in {filedir}")
+        
